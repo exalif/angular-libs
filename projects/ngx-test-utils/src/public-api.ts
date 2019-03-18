174b098b485e5dc3c4223e6536862060ab33b573
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2019-03-18 08:20:35 -0400

    refact: remove jest utils since it won't work from lib

--- projects/ngx-test-utils/src/public-api.ts
+++ projects/ngx-test-utils/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ngx-test-utils
  */
 
-export { JestUtils } from './lib/jest.utils';
 export { NgxTestUtilsModule } from './lib/ngx-test-utils.module';
 export { MockStore } from './lib/mocks/store.mock';
 export { RouterLinkMockDirective } from './lib/mocks/router-link.mock';
