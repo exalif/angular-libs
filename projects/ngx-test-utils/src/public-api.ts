9cc26420eb7463e9f1887652be3e858891bcc7cd
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2019-03-20 12:14:14 -0400

    feat(test-utils): expose RouterLinkMockDirective (#7)
    * feat(test-utils): expose RouterLinkMockDirective

--- projects/ngx-test-utils/src/public-api.ts
+++ projects/ngx-test-utils/src/public-api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of ngx-test-utils
  */
 
-export { JestUtils } from './lib/jest.utils';
 export { NgxTestUtilsModule } from './lib/ngx-test-utils.module';
 export { MockStore } from './lib/mocks/store.mock';
+export { RouterLinkMockDirective } from './lib/mocks/router-link.mock';
 export { assertTranslateWithParams } from './lib/utils/assert-translate-with-params';
