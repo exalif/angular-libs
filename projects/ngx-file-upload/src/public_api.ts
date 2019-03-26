a242bf953d9b427683a68172df7e74f018757860
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2019-03-25 20:41:18 -0400

    fix: add missing models

--- projects/ngx-file-upload/src/public_api.ts
+++ projects/ngx-file-upload/src/public_api.ts
@@ -3,7 +3,10 @@
  */
 
 export { UploadxOptions } from './lib/models/upload-options';
+export { UploaderOptions } from './lib/models/uploader-options';
 export { UploadxControlEvent } from './lib/models/control-event';
+export { UploadAction } from './lib/models/upload-action';
+export { UploadStatus } from './lib/models/upload-status';
 export { UploadState } from './lib/models/upload-state';
 export { UploadItem } from './lib/models/upload-item';
 
