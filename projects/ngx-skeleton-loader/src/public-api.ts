2188957175931d8b43668c2818a19829eae78438
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2019-10-14 12:52:28 -0400

    feat: add skeleton lib

---
+++ projects/ngx-skeleton-loader/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of ngx-skeleton-loader
+ */
+
+export * from './lib/ngx-skeleton-loader.component';
+export * from './lib/ngx-skeleton-loader.module';
+export * from './lib/models/appearance';
+export * from './lib/models/skeleton';
