fc71c714d5f816a7b9408fb14fc58b2877e7ab60
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2020-04-04 07:44:14 -0400

    feat (ngx-mat-popover): add support for angular 9 refact(ngx-mat-popover): expose options for scss mixin

--- projects/ngx-mat-popover/src/lib/ngx-mat-popover.component.ts
+++ projects/ngx-mat-popover/src/lib/ngx-mat-popover.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, ViewChild, TemplateRef, ChangeDetectionStrategy } from '@angular/core';
-import { MenuPositionX, MenuPositionY, MatMenuTrigger } from '@angular/material';
+import { MenuPositionX, MenuPositionY, MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'ngx-mat-popover',
--- projects/ngx-mat-popover/src/lib/ngx-mat-popover.module.ts
+++ projects/ngx-mat-popover/src/lib/ngx-mat-popover.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { MatMenuModule } from '@angular/material';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { NgxMatPopoverComponent } from './ngx-mat-popover.component';
 
