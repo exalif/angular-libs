fa897cbe7de918011797a703dfc8a8db53776068
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2020-04-21 14:44:11 -0400

    add signature pad component

--- projects/ngx-signature-pad/src/lib/ngx-signature-pad.module.ts
+++ projects/ngx-signature-pad/src/lib/ngx-signature-pad.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
+
 import { NgxSignaturePadComponent } from './ngx-signature-pad.component';
 
-
-
 @NgModule({
-  declarations: [NgxSignaturePadComponent],
+  declarations: [
+    NgxSignaturePadComponent,
+  ],
   imports: [
   ],
-  exports: [NgxSignaturePadComponent]
+  exports: [
+    NgxSignaturePadComponent,
+  ]
 })
 export class NgxSignaturePadModule { }
--- projects/ngx-signature-pad/src/public-api.ts
+++ projects/ngx-signature-pad/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-signature-pad
  */
 
-export * from './lib/ngx-signature-pad.service';
+export * from './lib/models/signature-pad-options';
 export * from './lib/ngx-signature-pad.component';
 export * from './lib/ngx-signature-pad.module';
